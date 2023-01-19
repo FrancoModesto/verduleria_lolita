d80528d58385b8a5f94b2868561077ae81fa6f87
FrancoModesto <franco.modesto@hotmail.com>
2023-01-18 22:02:52 -0300

    Finalizado Carrito + Arreglos

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,10 @@ const carritoNumber = document.querySelector(".carrito-number")
 const carritoSlide = document.querySelector(".carrito")
 const listadoCarrito = document.querySelector(".listado-carrito")
 
+const carritoBtnEmpty = document.querySelector(".btn-empty")
+const carritoBtnBuy = document.querySelector(".btn-buy")
+const carritoPrice = document.querySelector(".total-price")
+
 
 //LISTADO DE PRODUCTOS
 function comparacion(a, b) {
@@ -31,7 +35,7 @@ function ordenarPorNombre(a, b) {
 }
 
 function darEventosAProds(array) {
-    const prodsBtns = document.querySelectorAll(".btn")
+    const prodsBtns = document.querySelectorAll(".btn-add")
 
     prodsBtns.forEach((prodHTML, i) => {
         prodHTML.addEventListener("click", () => {
@@ -49,7 +53,7 @@ function renderizarProductos(array) {
         div.innerHTML = `<div class="img">${prod.img}</div>
                         <h3>${prod.name}</h3>
                         <h4>$${prod.price}</h4>
-                        <div class="btn">Agregar al Carrito</div>`
+                        <div class="default-btn btn-add">Agregar al Carrito</div>`
         listado.append(div)
     })
     darEventosAProds(array)
@@ -62,6 +66,90 @@ async function obtenerProductos() {
 }
 
 obtenerProductos()
+
+
+//CARRITO
+carritoBtn.addEventListener("click", () => {
+    if (carritoSlide.style.top == "-1em") {
+        carritoSlide.style.top = "-100%"
+    } else {
+        carritoSlide.style.top = "-1em"
+    }
+})
+
+function removerDeCarrito(i, prodHTML) {
+    carrito.splice(i, 1)
+    renderizarCarrito(carrito)
+}
+
+function darEventosAProdsCarrito(array) {
+    const prodsCarritoBtns = document.querySelectorAll(".btn-rem")
+
+    prodsCarritoBtns.forEach((prodHTML, i) => {
+        prodHTML.addEventListener("click", () => {
+            removerDeCarrito(i, prodHTML)
+        })
+    })
+}
+
+function calcularTotal(array) {
+    if (array.length === 1) {
+        carritoPrice.innerHTML = `$${parseInt(array[0].price)}`
+    } else {
+        let total = 0
+        array.forEach(prod => {
+            total += Number(prod.price)
+        })
+        carritoPrice.innerHTML = `$${total}`
+    }
+}
+
+function renderizarCarrito(array) {
+    localStorage.setItem("carrito", JSON.stringify(array))
+    carritoNumber.innerHTML = `${array.length}`
+
+    if (array.length > 0) {
+        calcularTotal(array)
+        listadoCarrito.innerHTML = ""
+        array.sort(ordenarPorNombre)
+        array.forEach(prod => {
+            const div = document.createElement("div")
+            div.className = "producto-carrito"
+            div.innerHTML = `<div class="img">${prod.img}</div>
+                            <h3>${prod.name}</h3>
+                            <h4>$${prod.price}</h4>
+                            <div class="default-btn btn-rem">X</div>`
+            listadoCarrito.append(div)
+        })
+        darEventosAProdsCarrito(array)
+    } else {
+        carritoPrice.innerHTML = "$0"
+        listadoCarrito.innerHTML = `<h3 class="empty-text">EL CARRITO SE ENCUENTRA VACÍO</h3>`
+    }
+}
+
+function agregarACarrito(prod) {
+    carrito.push(prod)
+    renderizarCarrito(carrito)
+}
+
+carrito = JSON.parse(localStorage.getItem("carrito"))
+renderizarCarrito(carrito)
+
+
+//CARRITO - BTNS
+function vaciarCarrito() {
+    carrito = []
+    renderizarCarrito(carrito)
+}
+
+carritoBtnEmpty.addEventListener("click", () => {
+    vaciarCarrito()
+})
+
+carritoBtnBuy.addEventListener("click", () => {
+    vaciarCarrito()
+})
 
 
 //FILTRADO
@@ -90,55 +178,3 @@ inputPrices.addEventListener("change", () => {
 inputSearch.addEventListener("input", () => {
     filtrarProductos()
 })
-
-
-//CARRITO
-carritoBtn.addEventListener("click", () => {
-    if (carritoSlide.style.top == "-1em") {
-        carritoSlide.style.top = "-100%"
-    } else {
-        carritoSlide.style.top = "-1em"
-    }
-})
-
-function removerDeCarrito(i, prodHTML) {
-    carrito.splice(i, 1)
-    renderizarCarrito(carrito)
-}
-
-function darEventosAProdsCarrito(array) {
-    const prodsCarritoBtns = document.querySelectorAll(".btn-rem")
-
-    prodsCarritoBtns.forEach((prodHTML, i) => {
-        prodHTML.addEventListener("click", () => {
-            removerDeCarrito(i, prodHTML)
-        })
-    })
-}
-
-function renderizarCarrito(array) {
-    carritoNumber.innerHTML = `${carrito.length}`
-    if (carrito.length > 0) {
-        listadoCarrito.innerHTML = ""
-        array.sort(ordenarPorNombre)
-        array.forEach(prod => {
-            const div = document.createElement("div")
-            div.className = "producto-carrito"
-            div.innerHTML = `<div class="img">${prod.img}</div>
-                            <h3>${prod.name}</h3>
-                            <h4>$${prod.price}</h4>
-                            <div class="btn-rem">X</div>`
-            listadoCarrito.append(div)
-        })
-        darEventosAProdsCarrito(array)
-    } else {
-        listadoCarrito.innerHTML = `<h3 class="empty-text">EL CARRITO SE ENCUENTRA VACÍO</h3>`
-    }
-}
-
-renderizarCarrito(carrito)
-
-function agregarACarrito(prod) {
-    carrito.push(prod)
-    renderizarCarrito(carrito)
-}
